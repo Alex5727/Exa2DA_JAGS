ab0dd34c3aea3834a56dbff7818b963bdb0320e6
Alex5727 <alejandrogrjalva@gmail.com>
2024-12-09 10:42:31 -0700

    Agregar archivos de proyecto.

---
+++ Exa2DA_JAGS/VM/VMFreCar.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Exa2DA_JAGS.VM
+{
+    internal class VMFreCar : BaseViewModel
+    {
+        #region VARIABLES
+        int _cantidad;
+        string _frecuencia;
+        int _canfrecuencia;
+        int _can;
+
+        #endregion
+        #region CONSTRUCTOR
+        public VMFreCar(INavigation navigation)
+        {
+            Navigation = navigation;
+
+        }
+        #endregion
+        #region OBJETOS
+        public int Cantidad
+        {
+            get { return _cantidad; }
+            set { SetValue(ref _cantidad, value); }
+        }
+        public string Frecuencia
+        {
+            get { return _frecuencia; }
+            set { SetValue(ref _frecuencia, value); }
+        }
+        public int Canfrecuencia
+        {
+            get { return _can; }
+            set { SetValue(ref _can, value); }
+        }
+
+        public int Can
+        {
+            get { return _canfrecuencia; }
+            set { SetValue(ref _canfrecuencia, value); }
+        }
+
+
+        #endregion
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void CalcularFrecuencia()
+        {
+            int Canfrecuencia = Cantidad * 4;
+            if (Canfrecuencia <= 60)
+            {
+                Frecuencia = "Frecuencia Cardaquia baja";
+            }
+            else if (Canfrecuencia >= 100)
+            {
+                Frecuencia = "Frecuencia Cardaquia alta";
+            }
+            else
+            {
+                Frecuencia = "Frecuencia Cardaquia Normal";
+            }
+            Can = Canfrecuencia;
+        }
+        #endregion
+        #region COMANDOS
+        public ICommand ProcesoAsyncomand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand CalcularCommand => new Command(CalcularFrecuencia);
+        #endregion
+    }
+}
+
---
+++ Exa2DA_JAGS/Vistas/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Exa2DA_JAGS.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exa2DA_JAGS.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new VMFreCar(Navigation);
+        }
+    }
+}
